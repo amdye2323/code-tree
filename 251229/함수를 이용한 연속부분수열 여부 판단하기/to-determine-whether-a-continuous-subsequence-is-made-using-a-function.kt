854b3a0e43f60771422e9d37259503f9a3e03a87
Root <58718331+amdye2323@users.noreply.github.com>
2025-12-29 21:50:39 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 135ms, Memory: 12MB, Status: Runtime Error - Codetree

--- 251229/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.kt
+++ 251229/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.kt
@@ -9,21 +9,23 @@ fun main() {
 fun validate(aList: List<Int>,bList: List<Int>){
     var firstB = bList[0]
     var flag = false
+    var cnt = 0
     for (i in aList.indices){
         if (aList[i] == firstB){
-            var cnt = 0
+            
             for (j in bList){
                 if (j == aList[i+cnt]){
                     cnt ++
                 }else {
+                    cnt = 0
                     break
                 }
-            }
-            if (cnt == bList.size){
-                print("Yes")
-            }else {
-                print("No")
-            }
+            }          
         }
+    }
+    if (cnt == bList.size){
+        print("Yes")
+    }else {
+        print("No")
     }
 }
