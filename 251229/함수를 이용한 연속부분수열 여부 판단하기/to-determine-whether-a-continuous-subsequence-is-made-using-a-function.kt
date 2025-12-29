3ffb41f9dbcc8b959fc0cd814adddac06c648f1e
Root <58718331+amdye2323@users.noreply.github.com>
2025-12-29 21:47:32 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 129ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 251229/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.kt
@@ -0,0 +1,29 @@
+fun main() {
+    val (n1, n2) = readLine()!!.split(" ").map { it.toInt() }
+    val a = readLine()!!.split(" ").map { it.toInt() }
+    val b = readLine()!!.split(" ").map { it.toInt() }
+    
+    validate(a,b)
+}
+
+fun validate(aList: List<Int>,bList: List<Int>){
+    var firstB = bList[0]
+    var flag = false
+    for (i in aList.indices){
+        if (aList[i] == firstB){
+            var cnt = 0
+            for (j in bList){
+                if (j == aList[i+cnt]){
+                    cnt ++
+                }else {
+                    break
+                }
+            }
+            if (cnt == bList.size){
+                print("Yes")
+            }else {
+                print("No")
+            }
+        }
+    }
+}
