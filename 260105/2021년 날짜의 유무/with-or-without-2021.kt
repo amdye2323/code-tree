74aad9e1cf6a23979dd5dad8ab047ed0648edebf
Root <58718331+amdye2323@users.noreply.github.com>
2026-01-05 22:46:22 +0900

    Title: 2021년 날짜의 유무, Time: 124ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 260105/2021년 날짜의 유무/with-or-without-2021.kt
@@ -0,0 +1,30 @@
+import java.io.InputStreamReader
+import java.io.BufferedReader
+import java.util.StringTokenizer
+
+fun main() {
+    val (m, d) = readln().split(" ").map { it.toInt() }
+    var flag = false
+    for (i in 1 .. 12){
+        if (i == m){
+            var maxDay = 30
+            if (i == 2){ // 2월?
+                maxDay = 28
+            }else if (i % 2 == 1){ // 홀수 해?
+                maxDay = 31
+            }
+
+            for (j in 1 .. maxDay){
+                if (j == d){
+                    flag = true
+                }
+            }
+        }
+    }
+
+    if (flag == true){
+        print("Yes")
+    }else {
+        print("No")
+    }
+}
