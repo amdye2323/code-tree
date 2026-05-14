5c88d3a2c1e0aa08c1aaf7831b73c3b4a03c5469
Root <58718331+amdye2323@users.noreply.github.com>
2026-05-15 08:38:07 +0900

    [Passed] 그래프 탐색 (97ms, 19MB)

--- 260515/그래프 탐색/graph-traversal.py
+++ 260515/그래프 탐색/graph-traversal.py
@@ -1,35 +1,38 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-n, m = tuple(map(int, input().split()))
-# Please write your code here.
+n, m = map(int, input().split())
 
-graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n + 1)]
 
-visited = [ False for _ in range(n+1)]
+visited = [False] * (n + 1)
 
 v_cnt = 0
+
 
 def dfs(v):
     global v_cnt
 
     for cur in graph[v]:
+
         if visited[cur]:
             continue
+
         visited[cur] = True
-        v_cnt +=1
+        v_cnt += 1
+
         dfs(cur)
 
 
-
-for i in range(m):
-    v1, v2 = tuple(map(int, input().split()))
+for _ in range(m):
+    v1, v2 = map(int, input().split())
 
     graph[v1].append(v2)
     graph[v2].append(v1)
 
 
 visited[1] = True
+
 dfs(1)
 
 print(v_cnt)
