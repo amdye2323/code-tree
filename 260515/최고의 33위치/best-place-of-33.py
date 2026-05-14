69b570faa70bf433df35e57f5b8f3dd4dc8bea00
Root <58718331+amdye2323@users.noreply.github.com>
2026-05-15 06:53:12 +0900

    [Passed] 최고의 33위치 (46ms, 16MB)

---
+++ 260515/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+
+
+
+
+_max =0
+for i in range(n-2):
+    for j in range(n-2):
+        total = 0
+        
+        for x in range(i, i+3):
+            for y in range(j, j+3):
+                total += grid[x][y]
+        _max = max(_max, total)
+
+print(_max)
+    
